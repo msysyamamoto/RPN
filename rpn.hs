a80cfb34da244ea2160ec04d072d054170b66dc9
ymmtmsys <m2yamamoto@gmail.com>
2012-10-02 08:15:13 +0900

    run as interactive shell

--- rpn.hs
+++ rpn.hs
@@ -1,4 +1,23 @@
 import Control.Monad
+import Data.Char
+
+main :: IO ()
+main = do
+    runCalc $ Just []
+    return ()
+
+runCalc :: Maybe [Double] -> IO ()
+runCalc xs = do
+    print xs
+    line <- getLine
+    if toLowers line == "quit"
+        then return ()
+        else runCalc $ do
+            stack <- xs
+            foldingFunction stack line
+
+toLowers :: String -> String
+toLowers = map toLower
 
 solveRPN :: String -> Maybe Double
 solveRPN st = do
@@ -9,9 +28,9 @@ foldingFunction :: [Double] -> String -> Maybe [Double]
 foldingFunction (x:y:ys) "*"    = return $ (y * x) :ys
 foldingFunction (x:y:ys) "+"    = return $ (y + x) :ys
 foldingFunction (x:y:ys) "-"    = return $ (y - x) :ys
-foldingFunction (0:_:_)  "/"    = Nothing 
+foldingFunction (0:_:_)  "/"    = Nothing
 foldingFunction (x:y:ys) "/"    = return $ (y / x) :ys
-foldingFunction xs numberString = liftM (:xs) (readMaybe numberString) 
+foldingFunction xs numberString = liftM (:xs) (readMaybe numberString)
 
 readMaybe :: (Read a) => String -> Maybe a
 readMaybe st = case reads st of [(x, "")] -> Just x
