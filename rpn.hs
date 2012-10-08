b9ff5b55d4835ff3bdcabc088e760b22f007cbb1
ymmtmsys <m2yamamoto@gmail.com>
2012-10-08 14:12:46 +0900

    parseInput

--- rpn.hs
+++ rpn.hs
@@ -1,6 +1,8 @@
 import Control.Monad
 import Data.Char
 
+data Item a = Number Double | Operator (a -> a -> a)| Quit
+
 main :: IO ()
 main = do
     runCalc $ Just []
@@ -10,14 +12,22 @@ runCalc :: Maybe [Double] -> IO ()
 runCalc xs = do
     print xs
     line <- getLine
-    if isQuit line
-        then return ()
-        else runCalc $ do
+    case parseInput line of
+        Nothing   -> runCalc xs
+        Just Quit -> return () 
+        Just y    -> runCalc $ do
             stack <- xs
-            foldingFunction stack line
+            foldingFunction' stack y
 
 isQuit :: String -> Bool
 isQuit = ("quit" ==) . toLowers
+
+parseInput :: String -> Maybe (Item Double)
+parseInput ['*']  = return $ Operator (*)
+parseInput "quit" = return Quit
+parseInput str    = case readMaybe str of
+                        Just x  -> return $ Number (x ::Double)
+                        Nothing -> Nothing
 
 toLowers :: String -> String
 toLowers = map toLower
@@ -26,6 +36,10 @@ solveRPN :: String -> Maybe Double
 solveRPN st = do
     [result] <- foldM foldingFunction [] $ words st
     return result
+
+foldingFunction' :: [Double] -> (Item Double) -> Maybe [Double]
+foldingFunction' (x:y:ys) (Operator op) = return $ (op y x) : ys 
+foldingFunction' xs (Number x) = return (x:xs)
 
 foldingFunction :: [Double] -> String -> Maybe [Double]
 foldingFunction (x:y:ys) "*"    = return $ (y * x) :ys
